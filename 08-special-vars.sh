b22df1ef3fe033dc2794c93cf51b5e61cb01ad2c
Jayani22 <mutcherlajayani@gmail.com>
2025-10-06 10:12:42 +0530

    variables added

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,3 +7,5 @@ echo "Current directory: $PWD"
 echo "Who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "Process Instance ID of this script: $$"
+sleep 50 &
+echo "PID Of the last command in background is :$!"
