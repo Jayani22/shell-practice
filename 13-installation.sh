90d4873bab00c4e91427c570481a5ece6c913896
Jayani22 <mutcherlajayani@gmail.com>
2025-10-06 19:43:40 +0530

    variables added

---
+++ 13-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privilage"
+    exit 1 # failure is other than 0
+f1
+
+VALIDATE(){ # functions receive inputs through args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+    exit 1
+    else
+        echo "Installing $2 is success"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
