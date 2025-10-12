7d2f8a8c5d55b34b241d1fb98870a92b1b5d6617
Jayani22 <mutcherlajayani@gmail.com>
2025-10-12 06:17:02 +0530

    logs

---
+++ 23-backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1
+fi
+
+USAGE(){
+    echo "USAGE:: sudo sh 23-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]"
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi
