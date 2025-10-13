8f8e0944fbdb2ced459c33dc79abcc16bf1b04b6
Jayani22 <mutcherlajayani@gmail.com>
2025-10-13 09:15:46 +0530

    Session 24

--- 24-cpu-usage.sh
+++ 24-cpu-usage.sh
@@ -5,13 +5,11 @@ DISK_THRESHOLD=2 # in project we keep it as 75
 IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 
-
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High Usage on $PARTITION: $USAGE"
         MESSAGE="High Disk usage on $PARTITION: $USAGE %"
     fi
 done <<< $DISK_USAGE
